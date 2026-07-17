b3fc3c4e8a72c3b48048cc2bb3c34b258aa3865e
Manshacu <manshacu@gmail.com>
2026-07-17 23:35:31 +0500

    Initial project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("=" * 40)
+    print("Welcome to Mansha AI Solutions")
+    print("Project : EduTrack AI")
+    print("Developer : Muhammad Mansha")
+    print("=" * 40)
+
+if __name__ == "__main__":
+    main()
