359bbe661fe38e68d08f5b9703c3e354b860bc4e
Manshacu <manshacu@gmail.com>
2026-07-19 14:32:57 -0700

    complete day 2 practice veriables#

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
+    developer = "Muhammad mansha"
+    age=34
     print("=" * 40)
     print("Welcome to Mansha AI Solutions")
     print("Project : EduTrack AI")
-    print("Developer : Muhammad Mansha")
+    print(developer)
+    print("age=",age)
     print("=" * 40)
 
 if __name__ == "__main__":
---
+++ student_info.py
@@ -0,0 +1,20 @@
+student_name="Muhammad Mansha"
+student_age=34
+student_city="Lahore"
+student_course="BSSE"
+student_cgpa= 3.57
+is_it_graduated= True
+
+print(student_name)
+print(student_age)
+print(student_city)
+print(student_course)
+print(student_cgpa)
+print(is_it_graduated)
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_city))
+print(type(student_course))
+print(type(student_cgpa))
+print(type(is_it_graduated))
